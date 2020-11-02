6e3367fd539481b80c3df74a134e7371050db28d
Dmitry Sychugov <dmitry.s.edu@gmail.com>
2020-11-02 19:28:56 +0500

    Updates

---
+++ precache-manifest.c9555d7920d720c13f8ecb81f173bb5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e91d5db12ae0fc6976dfd5054a0ccc",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "c93bf32ed5b11602ef30",
+    "url": "/weatherApp/static/js/2.476c9a50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weatherApp/static/js/2.476c9a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b868be70e68b1dd11b37",
+    "url": "/weatherApp/static/js/main.4d75adeb.chunk.js"
+  },
+  {
+    "revision": "2e2a161599f0090ccea8",
+    "url": "/weatherApp/static/js/runtime-main.1a8c77f4.js"
+  },
+  {
+    "revision": "63a7b614b50a1d92d1c5963dad86f68f",
+    "url": "/weatherApp/static/media/bg.63a7b614.jpg"
+  }
+]);
